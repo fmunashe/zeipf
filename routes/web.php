180d7f53ac4ce942221dd4e41cb752b72ceb5ca7
farai.zihove <farai.zihove@econet.co.zw>
2024-06-17 22:02:32 +0200

    remove duplicate route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\UssdBackendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('v1/')->group(function () {
-    Route::post('zeipf/backend/live', [UssdBackendController::class, 'DataProcessing'])->name("ZeipfBackendRoute");
-});
