930fd02fed06bc85785c77450e88e289ae8769a2
farai.zihove <farai.zihove@econet.co.zw>
2024-06-17 09:00:01 +0200

    initial commit

--- database/seeders/AccessTokenSeeder.php
+++ database/seeders/AccessTokenSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\AccessToken;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use Nette\Utils\Random;
 
 class AccessTokenSeeder extends Seeder
@@ -15,8 +16,8 @@ class AccessTokenSeeder extends Seeder
     public function run(): void
     {
         $accessTokens = [
-            ['id' => 1, 'app_name' => 'Zesa Ussd', 'protocol_id' => 2, 'token' => Random::generate(), 'expiration_date' => Carbon::now()->addYears(2), 'short_code' => '543', 'status' => 1],
-            ['id' => 2, 'app_name' => 'Zesa Ussd', 'protocol_id' => 1, 'token' => Random::generate(), 'expiration_date' => Carbon::now()->addYears(2), 'short_code' => '543', 'status' => 1]
+            ['id' => 1, 'app_name' => 'Zesa Ussd', 'protocol_id' => 2, 'token' => Str::random(50), 'expiration_date' => Carbon::now()->addYears(2), 'short_code' => '543', 'status' => 1],
+            ['id' => 2, 'app_name' => 'Zesa Ussd', 'protocol_id' => 1, 'token' => Str::random(50), 'expiration_date' => Carbon::now()->addYears(2), 'short_code' => '543', 'status' => 1]
         ];
         foreach ($accessTokens as $token) {
             AccessToken::query()->create($token);
